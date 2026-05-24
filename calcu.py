d70a977fdaf96ef2f76bb5a720c970d9f218e563
foluso <folusoosibodu7@gmail.com>
2026-05-24 02:41:14 +0100

    adding an operation menue

--- calcu.py
+++ calcu.py
@@ -1,34 +1,36 @@
 
 while True:
-  num1 = input("Enter the first number: ")
-  num2 = input("Enter the second number: ")
+    print("please select an operation:")
+    print("1- Addition")
+    print("2- Subtraction")
+    print("3- Multiplication")
+    print("4- Division")
+    
+    operation = input("operation?")
+    num1 = input("Enter the first number: ")
+    num2 = input("Enter the second number: ")
 
-  num1 = float(num1)
-  num2 = float(num2)
+    num1 = float(num1)
+    num2 = float(num2)
 
 
+    if operation == "1":
+        result = num1 + num2
+        print("The result is: ", result)
+    elif operation == "2":
+        result = num1 - num2
+        print("The result is: ", result)
+    elif operation == "3":
+        result = num1 * num2
+        print("The result is: ", result)
+    elif operation == "4":
+        if num2 == 0:
+            print("Error: Division by zero is not allowed.")
+        else:
+            result = num1 / num2
+            print("The result is: ", result)
 
-  print("Select operation:")
-  operation = input("operation?")   
-  operation = str(operation)
+    again = input("Do you want to perform another calculation? (yes/no)")
 
-  if operation == "+":
-      result = num1 + num2
-      print("The result is: ", result)
-  elif operation == "-":
-      result = num1 - num2
-      print("The result is: ", result)
-  elif operation == "*":
-      result = num1 * num2
-      print("The result is: ", result)
-  elif operation == "/":
-      if num2 == 0: 
-          print("Error: Division by zero is not allowed.")
-      else:
-          result = num1 / num2
-          print("The result is: ", result)
-
-  again = input("Do you want to perform another calculation? (yes/no)")
-
-  if again.lower() == "no":
-      break
+    if again.lower() == "no":
+        break
