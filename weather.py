e0e38515312daec8fed55dd00cf1719dc4660002
foluso <folusoosibodu7@gmail.com>
2026-05-30 01:07:36 +0100

    working weather code

--- weather.py
+++ weather.py
@@ -1,6 +1,15 @@
 import requests
 
-response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=6.5244&longitude=3.3792&current_weather=true")
+city = input("Enter your city: ")
+geo_url = "https://geocoding-api.open-meteo.com/v1/search?name=" + city + "&count=1"
+geo_response = requests.get(geo_url)
+geo_data = geo_response.json()
+
+lat = geo_data["results"][0]["latitude"]
+lon = geo_data["results"][0]["longitude"]
+
+
+response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=" + str(lat) + "&longitude=" + str(lon) + "&current_weather=true")
 data = response.json()
 
 print("temperature:", data["current_weather"]["temperature"])
